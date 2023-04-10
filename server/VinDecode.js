bf10eb286f6dd339b5aad0d58947319eb2af93e4
David T <114833245+tridav3@users.noreply.github.com>
2023-04-10 14:37:31 -0700

    added vin, added users

--- server/VinDecode.js
+++ server/VinDecode.js
@@ -1,10 +1,9 @@
-const fetch = require("node-fetch");
-
 const VIN_API_URL = "https://vpic.nhtsa.dot.gov/api/vehicles";
 
 const decodeVIN = async (vin, modelYear) => {
+  const fetch = await import("node-fetch");
   const url = `${VIN_API_URL}/DecodeVinValues/${vin}?format=json&modelyear=${modelYear}`;
-  const response = await fetch(url);
+  const response = await fetch.default(url);
   const data = await response.json();
   return data;
 };
--- server/server.js
+++ server/server.js
@@ -1,29 +1,80 @@
 const express = require("express");
 const app = express();
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { decodeVIN } = require("./VinDecode");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const client = new MongoClient(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
 });
 
-client.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MongoDB");
+app.use(express.json());
+
+//list of databases
+const listDatabases = async (client) => {
+  const databasesList = await client.db().admin().listDatabases();
+  const databaseNames = databasesList.databases.map((db) => db.name);
+  console.log("Databases:", databaseNames);
+  return databaseNames;
+};
+//connecting to the database
+app.get("/", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const databasesList = await listDatabases(client);
+    const usersCollection = client.db("VinDecode").collection("users");
+    const vinCollection = client.db("VinDecode").collection("Vin");
+
+    const users = await usersCollection.find().toArray();
+    const vinData = await vinCollection.find().toArray();
+
+    res.send({ databasesList, users, vinData });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+    console.log("Connection to MongoDB closed");
+  }
+});
+//posting vin data to the database
+app.post("/vin", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const usersCollection = client.db("VinDecode").collection("users");
+    const vinCollection = client.db("VinDecode").collection("Vin");
+
+    // Retrieve VIN number and model year
+    const { vin, modelYear, userId } = req.body;
+
+    // Use the VIN decoding, retrieve VIN data from API
+    const decodedVIN = await decodeVIN(vin, modelYear);
+
+    // Insert VIN data into  "Vin" collection
+    const result = await vinCollection.insertOne(decodedVIN);
+
+    // Add the VIN ID to the user's document in the "users"
+    await usersCollection.updateOne(
+      { _id: ObjectId(userId) },
+      { $push: { vins: decodedVIN.SearchCriteria } }
+    );
+
+    // Return the result of the insertion as a JSON object
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+    console.log("Connection to MongoDB closed");
+  }
 });
 
-app.get("/", (req, res) => {
-  const collection = client.db("VinDecode").collection("User");
-
-  collection.find().toArray((err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
+//
+//listening to the port so i can see on insomnia
 app.listen(8000, () => {
   console.log("Server listening on port 8000");
 });
